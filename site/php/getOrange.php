45f55d591dd4fb620ff0bc4ff3310ebcf374cbda
Valentin Sasyan <valentin.sasyan@gmail.com>
2016-11-20 02:37:09 +0100

    Affichage flèches ok, reste à voir comment gérer la taille...

--- site/php/getOrange.php
+++ site/php/getOrange.php
@@ -20,7 +20,7 @@
 			$orange['gens'][] = array(floatval($ligne['lon']), floatval($ligne['lat']), intval($ligne['nb_gens']));
 		}
 
-		$reponse = $bdd->query('SELECT origine.id_orange as origine_id, origine.lat as origine_lat, origine.lon as origine_lon, desti.id_orange as desti_id, desti.lat as desti_lat, desti.lon as desti_lon, flux.nb_gens as nb_gens, flux.datetime as datetime FROM `flux` INNER JOIN antenne AS origine ON flux.from_antenne = origine.id_orange INNER JOIN antenne AS desti ON flux.to_antenne = desti.id_orange WHERE flux.datetime BETWEEN timestamp(DATE_SUB(now(), INTERVAL 11 MINUTE)) AND timestamp(NOW()) AND nb_gens > 3 ORDER BY origine_id, desti_id');
+		$reponse = $bdd->query('SELECT origine.id_orange as origine_id, origine.lat as origine_lat, origine.lon as origine_lon, desti.id_orange as desti_id, desti.lat as desti_lat, desti.lon as desti_lon, flux.nb_gens as nb_gens, flux.datetime as datetime FROM `flux` INNER JOIN antenne AS origine ON flux.from_antenne = origine.id_orange INNER JOIN antenne AS desti ON flux.to_antenne = desti.id_orange WHERE flux.datetime BETWEEN timestamp(DATE_SUB(now(), INTERVAL 11 MINUTE)) AND timestamp(NOW()) AND nb_gens ORDER BY origine_id, desti_id');
 
 		$temp = array();
 		while ($ligne = $reponse->fetch()) {
@@ -36,7 +36,7 @@
 
 			if ($from_id < $to_id) {
 				// Necessairement le premier cas, on ajout cache.
-				if (!array_key_exists($from_id, $temp)) {$temp[$from_id] == array();}
+				if (!array_key_exists($from_id, $temp)) {$temp[$from_id] = array();}
 				$temp[$from_id][$to_id] = array(
 					'from' => $from,
 					'to' => $to,
@@ -44,9 +44,9 @@
 				);
 			} else {
 				// Premier cas ?
-				if (!array_key_exists($to_id, $temp) && !array_key_exists($from_id, $temp[$to_id])) {
+				if (!array_key_exists($to_id, $temp) || !array_key_exists($from_id, $temp[$to_id])) {
 					// Premier cas, on ajoute cache :
-					if (!array_key_exists($from_id, $temp)) {$temp[$from_id] == array();}
+					if (!array_key_exists($from_id, $temp)) {$temp[$from_id] = array();}
 					$temp[$from_id][$to_id] = array(
 						'from' => $from,
 						'to' => $to,
@@ -54,14 +54,35 @@
 					);
 				} else {
 					// Il faut comparer avec l'autre version :
-					if ($temp[$to_id])
+					if ($temp[$to_id][$from_id]['nb_gens'] < intval($ligne['nb_gens'])) {
+						// On ajoute l'autre sens moins le premier sens :
+						if (!array_key_exists($from_id, $temp)) {$temp[$from_id] = array();}
+						$temp[$from_id][$to_id] = array(
+							'from' => $from,
+							'to' => $to,
+							'nb_gens' => intval($ligne['nb_gens']) - $temp[$to_id][$from_id]['nb_gens']
+						);
+						// On supprime ce sens :
+						$temp[$to_id][$from_id]['nb_gens'] = 0;
+					} else if ($temp[$to_id][$from_id]['nb_gens'] > intval($ligne['nb_gens'])) {
+						// On diminue nb_gens :
+						$temp[$to_id][$from_id]['nb_gens'] -= intval($ligne['nb_gens']);
+					} else {
+						// Egale, on supprime :
+						$temp[$to_id][$from_id]['nb_gens'] = 0;
+					}
 				}
 			}
-
-
 		}
 
-		$orange['flux']
+		// On remet temps dans le bon sens :
+		foreach ($temp as $from_id => $from) {
+			foreach ($from as $to_id => $to) {
+				if ($to['nb_gens'] > 3) {
+					$orange['flux'][] = $to;
+				}
+			}
+		}
 
 		return $orange;
 	}
